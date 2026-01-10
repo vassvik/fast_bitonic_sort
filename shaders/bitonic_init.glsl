950985ec2a73ab5a7e93cac32f8db72e97aa61e1
Morten Vassvik <mvassvik@gmail.com>
2026-01-10 22:29:42 +0100

    init

--- shaders/bitonic_init.glsl
+++ shaders/bitonic_init.glsl
@@ -24,6 +24,6 @@ uvec4 pcg4d(uvec4 v) {
 
 void main() {
     uint gid = gl_GlobalInvocationID.x;
-    b_values[gid] = 2*(gid&32767) + (gid/32768);
+    b_values[gid] = 2*(gid&16383) + (gid/16384);
 }
 
